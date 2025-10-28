39d113bc16df50ee62c2722446d726f7378a9fd1
ziaulkadri <ziaulkadri1600@gmail.com>
2025-10-28 23:38:37 +0530

    added usersTable

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,16 @@
-import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core"
+import { pgTable, varchar, uuid, text, timestamp } from "drizzle-orm/pg-core"
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
+
+  firstName: varchar("first_name", { length: 55 }).notNull(),
+  lastName: varchar("last_name", { length: 55 }),
+
+  email: varchar({ length: 255 }).notNull().unique(),
+
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
 })
